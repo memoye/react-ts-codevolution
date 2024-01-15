fc42d47bc34dfd1a9781322239911b84e1b29cbd
Brown <memoyebrown@gmail.com>
2024-01-15 05:24:16 +0100

    polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ import "./App.css";
 import { List } from "components/generics/List";
 import { RandomNumber } from "components/restriction/RandomNumber";
 import { Toast } from "components/templateLiterals/Toast";
-import { CustomButton } from "./components/html/Button";
+import { CustomButton } from "components/html/Button";
+import { Text } from "components/polymorphic/Text";
 
 function App() {
   // const personName = {
@@ -44,6 +45,18 @@ function App() {
       <RandomNumber value={10} isPositive={true} />
       <Toast position={"center"} />
       <CustomButton variant={"primary"}>Label</CustomButton>
+
+      <div>
+        <Text as="h1" size="lg">
+          Heading
+        </Text>
+        <Text as="p" size="md">
+          Paragraph
+        </Text>
+        <Text as="label" htmlFor="someId" size="sm" color="secondary">
+          label
+        </Text>
+      </div>
 
       {/* <Greet name={"Brown"} messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
